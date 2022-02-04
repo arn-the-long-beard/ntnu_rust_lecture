f625cced2cee4a0063d42f432cdd3ee272348293
arn-the-long-beard <arn.menant@gmail.com>
2022-02-05 00:37:28 +0100

    fix: try to balance the fight

--- examples/rpg/src/main.rs
+++ examples/rpg/src/main.rs
@@ -19,7 +19,11 @@ fn main() {
 
     let iron_shield = Shield::new("Iron Shield", 25.0, 5.0);
     let steel_shield = Shield::new("steal Shield", 35.0, 7.0);
-    let daedric_armor = BodyArmor::new("Daedric Shield", 45.0);
+
+    let daedric_armor = BodyArmor::new("Daedric Armor", 45.0);
+
+    let daedric_armor_2 = BodyArmor::new("Daedric Armor 2", 45.0);
+
     let steel_armor = BodyArmor::new("Steel armor", 35.0);
     let iron_armor = BodyArmor::new("Iron Armor", 30.0);
     let grand_ma_skyrim = Character::new("Skyrim Grandma", 1500.00)
@@ -28,8 +32,8 @@ fn main() {
 
     let white_run_guard = Character::new("Olaf the dummy guard", 1500.00)
         .grab_weapon(iron_long_sword)
-        .grab_weapon(iron_shield)
-        .grab_armor(steel_armor);
+        .grab_weapon(steel_shield)
+        .grab_armor(daedric_armor_2);
 
     Fight::new(grand_ma_skyrim, white_run_guard).start();
 }
