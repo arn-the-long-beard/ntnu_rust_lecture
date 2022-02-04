100efe797487e2c0854ebf26a999361587775ec5
arn-the-long-beard <arn.menant@gmail.com>
2022-02-04 18:14:38 +0100

    feature: added full handling for weapon equipping

--- examples/rpg/src/stuff.rs
+++ examples/rpg/src/stuff.rs
@@ -2,6 +2,7 @@ use crate::item::{Armor, HandheldType, Weapon};
 use std::rc::Rc;
 
 /// https://stackoverflow.com/questions/49377231/when-to-use-rc-vs-box
+/// Option on Rc is maybe useless, I could use default value for Weapon as well.
 #[derive(Default)]
 pub struct Stuff {
     armor: Option<Rc<dyn Armor>>,
@@ -14,36 +15,37 @@ impl Stuff {
         self.armor = armor;
     }
 
-    pub fn set_first_weapon<W: 'static + Weapon>(&mut self, first_weapon: W) {
+    fn set_first_weapon<W: 'static + Weapon>(&mut self, first_weapon: W) {
         self.first_weapon = Some(Rc::new(first_weapon));
     }
 
-    pub fn set_second_weapon<W: 'static + Weapon>(&mut self, second_weapon: W) {
+    fn set_second_weapon<W: 'static + Weapon>(&mut self, second_weapon: W) {
         self.second_weapon = Some(Rc::new(second_weapon))
     }
 
-    pub fn unset_first_weapon(mut self) -> Self {
+    fn unset_first_weapon(&mut self) {
         self.first_weapon = None;
-        self
     }
 
-    pub fn unset_second_weapon(mut self) -> Self {
+    fn unset_second_weapon(&mut self) {
         self.second_weapon = None;
-        self
     }
 
     pub fn armor(&self) -> &Option<Rc<dyn Armor>> {
         &self.armor
     }
 
-    pub fn first_weapon(&self) -> &Option<Rc<dyn Weapon>> {
+    fn first_weapon(&self) -> &Option<Rc<dyn Weapon>> {
         &self.first_weapon
     }
 
-    pub fn second_weapon(&self) -> &Option<Rc<dyn Weapon>> {
+    fn second_weapon(&self) -> &Option<Rc<dyn Weapon>> {
         &self.second_weapon
     }
-
+    /// Will panic if you have equipped a two hand weapon as a second Weapon.
+    /// We could have specific trait for weapons to be used with both Hands.
+    /// Ex : SingleHand Item could have a trait "BothHand", and restrict this trait for second hand.
+    ///
     pub fn add_weapon<W: 'static + Weapon>(mut self, weapon: W) -> Self {
         match weapon.handheld_type() {
             HandheldType::SingleHand => {
@@ -54,8 +56,27 @@ impl Stuff {
                 }
                 self.set_first_weapon(weapon);
             }
-            HandheldType::OnlyLeft => {}
-            HandheldType::TwoHands => {}
+            HandheldType::OnlyLeft => {
+                if let Some(current_first_weapon) = self.first_weapon() {
+                    if current_first_weapon.handheld_type() == &HandheldType::TwoHands {
+                        self.unset_first_weapon();
+                    }
+                }
+
+                // See comment on how we could avoid this issue at compile time.
+                // if First weapon is set or not, we do not care, left item always goes left.
+                if let Some(current_second_weapon) = self.second_weapon() {
+                    if current_second_weapon.handheld_type() == &HandheldType::TwoHands {
+                        panic!("It seems you have a two hand weapon as second weapon");
+                    }
+                }
+
+                self.set_second_weapon(weapon)
+            }
+            HandheldType::TwoHands => {
+                self.unset_second_weapon();
+                self.set_first_weapon(weapon);
+            }
         }
 
         self
@@ -78,15 +99,17 @@ impl Stuff {
 
 #[cfg(test)]
 mod test {
-    use crate::item::{HandheldType, RegularWeapon, Shield};
+    use crate::item::{HandheldType, Item, RegularWeapon, Shield};
     use crate::stuff::Stuff;
     use std::rc::Rc;
 
     fn get_long_iron_sword() -> RegularWeapon {
         RegularWeapon::new("Long Iron Sword", 25.0, HandheldType::SingleHand)
     }
-
-    fn get_steel_battle_exe() -> RegularWeapon {
+    fn get_long_steel_sword() -> RegularWeapon {
+        RegularWeapon::new("Long Steel Sword", 30.0, HandheldType::SingleHand)
+    }
+    fn get_steel_battle_axe() -> RegularWeapon {
         RegularWeapon::new("Steal battle Axe", 65.0, HandheldType::TwoHands)
     }
 
@@ -99,88 +122,106 @@ mod test {
     }
 
     #[test]
-    fn get_stuff() {
-        let iron_long_sword = get_long_iron_sword();
+    /// Let's see when we move the first weapon as second
+    /// Let's see if using just one weapon works
+    fn use_single_hand_weapon() {
+        let long_iron_sword = get_long_iron_sword();
+        let long_steel_sword = get_long_steel_sword();
         let steel_shield = get_steel_shield();
-        let iron_shield = get_iron_shield();
 
         let mut stuff = Stuff::new(
             None,
-            Some(Rc::new(iron_long_sword)),
+            Some(Rc::new(long_iron_sword)),
             Some(Rc::new(steel_shield)),
         );
 
         assert_eq!(
             stuff.first_weapon.as_ref().unwrap().name().to_string(),
-            "Long Iron Sword"
+            get_long_iron_sword().name()
         );
 
         assert_eq!(
             stuff.second_weapon.as_ref().unwrap().name().to_string(),
-            "Steel Shield"
+            get_steel_shield().name()
         );
 
-        stuff.set_second_weapon(iron_shield);
+        stuff = stuff.add_weapon(long_steel_sword);
 
         assert_eq!(
             stuff.first_weapon.as_ref().unwrap().name().to_string(),
-            "Long Iron Sword"
+            get_long_steel_sword().name()
         );
 
         assert_eq!(
             stuff.second_weapon.as_ref().unwrap().name().to_string(),
-            "Iron Shield"
+            get_long_iron_sword().name()
+        );
+
+        stuff.unset_first_weapon();
+
+        assert!(stuff.first_weapon.is_none());
+
+        let sword = get_long_steel_sword();
+        stuff = stuff.add_weapon(sword);
+
+        assert_eq!(
+            stuff.first_weapon.as_ref().unwrap().name().to_string(),
+            get_long_steel_sword().name()
+        );
+
+        assert_eq!(
+            stuff.second_weapon.as_ref().unwrap().name().to_string(),
+            get_long_iron_sword().name()
         );
     }
 
     #[test]
     fn get_single_hand_weapon() {
-        let iron_long_sword = get_long_iron_sword();
-        let another_iron_long_sword = get_long_iron_sword();
+        let long_iron_sword = get_long_iron_sword();
+        let another_long_iron_sword = get_long_iron_sword();
         let steel_shield = get_steel_shield();
-        let iron_shield = get_iron_shield();
 
         let mut stuff = Stuff::new(
             None,
-            Some(Rc::new(iron_long_sword)),
+            Some(Rc::new(long_iron_sword)),
             Some(Rc::new(steel_shield)),
         );
         // because of mut, need &mut self if we want to update partialy the object;
-        stuff = stuff.add_weapon(another_iron_long_sword);
+        stuff = stuff.add_weapon(another_long_iron_sword);
 
         assert_eq!(
             stuff.first_weapon.as_ref().unwrap().name().to_string(),
-            "Long Iron Sword"
+            get_long_iron_sword().name()
         );
 
         assert_eq!(
             stuff.second_weapon.as_ref().unwrap().name().to_string(),
-            "Long Iron Sword"
+            get_long_iron_sword().name()
         );
     }
 
     #[test]
     fn replace_two_hands_weapon_with_single() {
-        let steel_battle_axe = get_steel_battle_exe();
+        let steel_battle_axe = get_steel_battle_axe();
         let mut stuff = Stuff::new(None, Some(Rc::new(steel_battle_axe)), None);
-        let iron_long_sword = get_long_iron_sword();
-        stuff = stuff.add_weapon(iron_long_sword);
+        let long_iron_sword = get_long_iron_sword();
+        stuff = stuff.add_weapon(long_iron_sword);
         assert_eq!(
             stuff.first_weapon.as_ref().unwrap().name().to_string(),
-            "Long Iron Sword"
+            get_long_iron_sword().name()
         );
         assert!(stuff.second_weapon.is_none());
     }
 
+    #[test]
     fn get_two_hands_weapons() {
-        let steel_battle_axe = get_steel_battle_exe();
-        let iron_long_sword = get_long_iron_sword();
-        let another_iron_long_sword = get_long_iron_sword();
+        let steel_battle_axe = get_steel_battle_axe();
+        let long_iron_sword = get_long_iron_sword();
         let steel_shield = get_steel_shield();
 
         let mut stuff = Stuff::new(
             None,
-            Some(Rc::new(iron_long_sword)),
+            Some(Rc::new(long_iron_sword)),
             Some(Rc::new(steel_shield)),
         );
 
@@ -188,9 +229,39 @@ mod test {
 
         assert_eq!(
             stuff.first_weapon.as_ref().unwrap().name().to_string(),
-            "Steal battle Axe"
+            get_steel_battle_axe().name()
         );
 
         assert!(stuff.second_weapon.is_none());
+    }
+
+    #[test]
+    fn get_shields() {
+        let long_iron_sword = get_long_iron_sword();
+        let another_long_iron_sword = get_long_iron_sword();
+        let steel_shield = get_steel_shield();
+        let iron_shield = get_iron_shield();
+
+        let mut stuff = Stuff::new(
+            None,
+            Some(Rc::new(long_iron_sword)),
+            Some(Rc::new(steel_shield)),
+        );
+
+        stuff = stuff.add_weapon(iron_shield);
+
+        assert_eq!(
+            stuff.second_weapon.as_ref().unwrap().name().to_string(),
+            get_iron_shield().name()
+        );
+
+        stuff = stuff
+            .add_weapon(get_iron_shield())
+            .add_weapon(get_steel_shield());
+
+        assert_eq!(
+            stuff.second_weapon.as_ref().unwrap().name().to_string(),
+            get_steel_shield().name()
+        );
     }
 }
