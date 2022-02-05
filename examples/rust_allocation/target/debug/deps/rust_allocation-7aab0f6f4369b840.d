e6e779455ae0511935f16c940515f4abec37e229
arn-the-long-beard <arn.menant@gmail.com>
2022-02-05 12:06:16 +0100

    fix: removed target files

--- examples/rust_allocation/target/debug/deps/rust_allocation-7aab0f6f4369b840.d
+++
@@ -1,5 +0,0 @@
-/home/arnaud/Projects/ntnu_rust_lecture/examples/rust_allocation/target/debug/deps/rust_allocation-7aab0f6f4369b840: src/main.rs
-
-/home/arnaud/Projects/ntnu_rust_lecture/examples/rust_allocation/target/debug/deps/rust_allocation-7aab0f6f4369b840.d: src/main.rs
-
-src/main.rs:
--- examples/rust_allocation/target/debug/rust_allocation.d
+++
@@ -1,1 +0,0 @@
-/home/arnaud/Projects/ntnu_rust_lecture/examples/rust_allocation/target/debug/rust_allocation: /home/arnaud/Projects/ntnu_rust_lecture/examples/rust_allocation/src/main.rs
