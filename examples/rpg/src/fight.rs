190b2206acd81cdb9dba243d155a6b5bb54ef187
arn-the-long-beard <arn.menant@gmail.com>
2022-02-05 01:02:04 +0100

    feature: corrected condition with equal so ratio is 50 / 50 for initiative and added attacks counting

--- examples/rpg/src/fight.rs
+++ examples/rpg/src/fight.rs
@@ -1,5 +1,6 @@
 use crate::character::{Character, HealthStatus};
 use crate::dice::SkillDice;
+use std::collections::HashMap;
 
 pub struct Fight {
     winner_name: Option<String>,
@@ -30,57 +31,84 @@ impl Fight {
     }
 
     pub fn start(&mut self) {
+        let mut attack_counting: HashMap<String, u32> = HashMap::new();
+        attack_counting.insert(self.opponents.1.name().to_string(), 0);
+        attack_counting.insert(self.opponents.0.name().to_string(), 0);
         while self.winner_name.is_none() {
             self.round += 1;
 
             println!("----------------- round : {} ----------------", self.round);
             // Initiative dice roll.
-            let (name1, name2) = self.roll_initiative_dice();
+            let (attacker_name, defender_name) = self.roll_initiative_dice();
 
-            let (fighter1, fighter2) = if self.opponents.1.name() == name1 {
+            let (attacker, defender) = if self.opponents.1.name() == attacker_name {
                 (&mut self.opponents.1, &mut self.opponents.0)
             } else {
                 (&mut self.opponents.0, &mut self.opponents.1)
             };
-            println!("{} will attack during this round", name1);
 
-            println!("{} will defend ", name2);
+            let number_attacks = &attack_counting
+                .get(&attacker_name)
+                .expect("Should have gotten number of attack for given name");
+
+            let new_nb = *number_attacks + 1;
+            attack_counting.insert(attacker_name.clone(), new_nb);
+
+            println!("{} will attack during this round", attacker_name.clone());
+
+            println!("{} will defend ", defender_name);
 
             // Attack & Dodge dice roll.
-            let attack = fighter1.roll_dice(SkillDice::Attack);
+            let attack = attacker.roll_dice(SkillDice::Attack);
 
-            let dodge = fighter2.roll_dice(SkillDice::Dodge);
+            let dodge = defender.roll_dice(SkillDice::Dodge);
 
             if dodge > attack {
                 println!(
                     "{} dodged {} attacks successfully",
-                    fighter2.name(),
-                    fighter1.name()
+                    defender.name(),
+                    attacker.name()
                 );
             } else {
-                let damage = fighter1.deal_damages();
+                let damage = attacker.deal_damages();
                 // Block dice roll.
-                let def = fighter2
+                let def = defender
                     .can_block()
-                    .map(|_| fighter2.roll_dice(SkillDice::Blocking));
+                    .map(|_| defender.roll_dice(SkillDice::Blocking));
 
                 if def.is_some() {
-                    println!("{} tries to block {}", fighter2.name(), fighter1.name());
+                    println!("{} tries to block {}", defender.name(), attacker.name());
                 }
-                fighter2.get_attacked_by(damage, attack, def);
+                defender.get_attacked_by(damage, attack, def);
             }
 
-            if fighter2.get_health_status() == HealthStatus::Dead {
-                self.winner_name = Some(fighter1.name().to_string());
+            if defender.get_health_status() == HealthStatus::Dead {
+                self.winner_name = Some(attacker.name().to_string());
 
-                println!("{} is dead", fighter2.name());
+                println!("{} is dead", defender.name());
                 println!(
-                    "{} won the fight and has {} left",
-                    fighter1.name(),
-                    fighter1.health()
+                    "{} won the fight and has {} hp left",
+                    attacker.name(),
+                    attacker.health()
                 );
 
                 println!("Fight finished after {} rounds", self.round);
+
+                let defender_nb_attacks = attack_counting
+                    .get(&defender_name)
+                    .expect("Should have gotten defender attack number");
+                let attacker_nb_attacks = attack_counting
+                    .get(&attacker_name)
+                    .expect("Should have gotten defender attack number");
+                println!(
+                    " {} has attacked {} times ",
+                    defender_name, defender_nb_attacks
+                );
+                println!(
+                    " {} has attacked {} times ",
+                    attacker_name, attacker_nb_attacks
+                );
+                println!()
             }
         }
     }
@@ -88,7 +116,7 @@ impl Fight {
     // Get the names of attacker and defender
     pub fn roll_initiative_dice(&self) -> (String, String) {
         if self.opponents.1.roll_dice(SkillDice::Initiative)
-            > self.opponents.0.roll_dice(SkillDice::Initiative)
+            >= self.opponents.0.roll_dice(SkillDice::Initiative)
         {
             (
                 self.opponents.1.name().to_string(),
