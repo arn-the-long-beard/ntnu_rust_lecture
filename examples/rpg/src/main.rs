ce2c23fade5e94284d98b8180a46ef4055e54377
arn-the-long-beard <arn.menant@gmail.com>
2022-02-05 15:48:58 +0100

    fix: try to make better balanced game

--- examples/rpg/src/main.rs
+++ examples/rpg/src/main.rs
@@ -11,31 +11,30 @@ use item::*;
 fn main() {
     println!("Hello and Fight");
 
+    // Lets put some armors.
     let iron_plate = BodyArmor::new("Iron Plate", 32.0);
     let steel_plate = BodyArmor::new("Steel Plate", 54.0);
+    let daedric_armor = BodyArmor::new("Daedric Armor", 25.0);
+    let daedric_armor_2 = BodyArmor::new("Daedric Armor 2", 25.0);
 
-    let iron_long_sword = RegularWeapon::new("Iron Long Sword", 55.0, HandheldType::SingleHand);
-    let steel_battle_axe = RegularWeapon::new("Steal battle Axe", 85.0, HandheldType::TwoHands);
+    // Lets put some shields
+    let steel_shield = Shield::new("steal Shield", 55.0, 20.0);
+    let iron_shield = Shield::new("Iron Shield", 25.0, 15.0);
 
-    let daedric_battle_axe =
-        RegularWeapon::new("Daedric battle Axe", 115.0, HandheldType::TwoHands);
+    // Lets put some weapons.
+    let iron_long_sword = RegularWeapon::new("Iron Long Sword", 35.0, HandheldType::SingleHand);
+    let steel_battle_axe = RegularWeapon::new("Steel battle Axe", 65.0, HandheldType::TwoHands);
+    let daedric_battle_axe = RegularWeapon::new("Daedric battle Axe", 85.0, HandheldType::TwoHands);
 
-    let iron_shield = Shield::new("Iron Shield", 25.0, 5.0);
-    let steel_shield = Shield::new("steal Shield", 35.0, 7.0);
-
-    let daedric_armor = BodyArmor::new("Daedric Armor", 45.0);
-
-    let daedric_armor_2 = BodyArmor::new("Daedric Armor 2", 45.0);
-
-    let steel_armor = BodyArmor::new("Steel armor", 35.0);
-    let iron_armor = BodyArmor::new("Iron Armor", 30.0);
-    let grand_ma_skyrim = Character::new("Skyrim Grandma", 1500.00)
+    let grand_ma_skyrim = Character::new("Skyrim Grandma", 300.00)
         .grab_weapon(steel_battle_axe)
         .grab_armor(daedric_armor);
 
-    let white_run_guard = Character::new("Olaf the dummy guard", 1500.00)
-        .grab_weapon(daedric_battle_axe)
+    let white_run_guard = Character::new("Olaf the dummy guard", 300.00)
+        .grab_weapon(steel_shield) // <- we can do it because of generic + trait objects for weapon
+        .grab_weapon(iron_long_sword)
         .grab_armor(daedric_armor_2);
+
     Fight::new(white_run_guard, grand_ma_skyrim).start();
 }
 
